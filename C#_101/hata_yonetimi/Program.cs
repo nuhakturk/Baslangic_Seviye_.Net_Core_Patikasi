7a4289dc694f511912b3d617427acf1c156ace54
Nuh Aktürk <nuhar42@gmail.com>
2021-11-27 19:55:38 +0300

    Hata Yonetimi ornekleri yapildi

--- C#_101/hata_yonetimi/Program.cs
+++ C#_101/hata_yonetimi/Program.cs
@@ -6,7 +6,45 @@ namespace hata_yonetimi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // try
+            // {
+            //     Console.WriteLine("Bir sayı giriniz: ");
+            //     int sayi = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+            // }
+            // catch(Exception ex)
+            // {
+            //     Console.WriteLine("Hata: " + ex.Message.ToString());
+            // }
+            // finally
+            // {
+            //     Console.WriteLine("İşlem tamamlandı.");
+            // }
+            
+            try
+            {
+                 //int a = int.Parse(null);
+                 //int a = int.Parse("test");
+                 int a = int.Parse("-12345678910");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş deger girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
         }
     }
 }
