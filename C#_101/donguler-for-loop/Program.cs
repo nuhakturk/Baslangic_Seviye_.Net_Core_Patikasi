3510168d0268d04fab580b767da6ca4c5a3d4e80
Nuh Aktürk <nuhar42@gmail.com>
2021-12-29 14:15:58 +0300

    .NET console projesi oluşturuldu

---
+++ C#_101/donguler-for-loop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
