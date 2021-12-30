db3e1cc9864e7755279a48e56c9a43ebc113c5a2
Nuh Aktürk <nuhar42@gmail.com>
2021-12-30 13:18:52 +0300

    .NET console projesi olusturuldu

---
+++ C#_101/diziler-array-sinifi-metodlari/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
