9522dd5bf08d2ccd20d4d4d918374bd9ab202c7f
Nuh Aktürk <nuhar42@gmail.com>
2021-12-30 13:26:50 +0300

    Sort metodu

--- C#_101/diziler-array-sinifi-metodlari/Program.cs
+++ C#_101/diziler-array-sinifi-metodlari/Program.cs
@@ -6,7 +6,22 @@ namespace diziler_array_sinifi_metodlari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Sort
+            int[] sayiDizisi = {23, 12, 4, 35, 86, 72, 3, 11, 7};
+
+            Console.WriteLine("******* Sırasız Dizi *******");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("******* Sıralı Dizi *******");
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
         }
     }
 }
