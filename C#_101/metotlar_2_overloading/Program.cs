3ed2fc9d411033c6121c52ddee0dc0babb857906
Nuh Aktürk <nuhar42@gmail.com>
2021-12-30 21:27:09 +0300

    metot-overloading ornekleri yapildi

--- C#_101/metotlar_2_overloading/Program.cs
+++ C#_101/metotlar_2_overloading/Program.cs
@@ -6,7 +6,53 @@ namespace metotlar_2_overloading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //out parametreler
+            string sayi = "999";
+            int outSayi;
+
+            bool sonuc = int.TryParse(sayi, out outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot Aşırı Yükleme - Overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Nuh","Aktürk");
+
+            //Metot Imzası
+            //metotAdı + parametre sayisi+ parametre
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
         }
     }
 }
