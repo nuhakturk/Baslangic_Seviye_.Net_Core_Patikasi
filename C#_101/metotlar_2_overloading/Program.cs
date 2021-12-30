9169a957aef08a325136ab8da2d8fb02ab649510
Nuh Aktürk <nuhar42@gmail.com>
2021-12-30 21:05:54 +0300

    .NET console projesi olusturuldu

---
+++ C#_101/metotlar_2_overloading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metotlar_2_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
