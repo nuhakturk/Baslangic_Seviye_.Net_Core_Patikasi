4f59ded394c75adbe0adca697f0bd6a4f6f32605
Nuh Aktürk <nuhar42@gmail.com>
2022-01-03 00:00:41 +0300

    .NET console projesi olusturuldu

---
+++ C#_101/recursive_extension_metotlar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace recursive_extension_metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
