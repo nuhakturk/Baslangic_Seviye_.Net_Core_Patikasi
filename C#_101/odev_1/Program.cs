782d90ff5e4c09f0dd4c2d481e52d473e8170e93
Nuh Aktürk <nuhar42@gmail.com>
2022-01-05 01:01:46 +0300

    1. Sorunun cevabi yapildi.

--- C#_101/odev_1/Program.cs
+++ C#_101/odev_1/Program.cs
@@ -6,7 +6,49 @@ namespace odev_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //************** 1. Soru **************
+            Console.WriteLine("************** 1. Soru **************");
+
+            uint n = 0;
+            do
+            {
+                try
+                {
+                    Console.Write("\nPozitif bir sayı giriniz: ");
+                    n = uint.Parse(Console.ReadLine());
+                    if (n == 0)
+                    {
+                        Console.WriteLine("0 Sayısını giremezsiniz Tekrar Deneyin!");
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("Hatalı bir giriş yaptınız. Hata: {0}", ex.Message);
+                }
+            } while (n < 1);
+
+            uint[] pozSayiDizisi = new uint[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}. Sayıyı giriniz: ", i + 1);
+                pozSayiDizisi[i] = uint.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Girmiş olduğunuz çift sayılar: \n");
+            foreach (var cift in pozSayiDizisi)
+            {
+                if (cift%2 == 0)
+                {
+                    Console.WriteLine("- " + cift);
+                }
+            }
+
+            //************** 2. Soru **************
+            Console.WriteLine("************** 2. Soru **************");
+            //************** 3. Soru **************
+            Console.WriteLine("************** 3. Soru **************");
+            //************** 4. Soru **************
+            Console.WriteLine("************** 4. Soru **************");
         }
     }
 }
