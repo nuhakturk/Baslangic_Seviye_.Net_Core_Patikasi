7f40deda5481f06dabfc854922629797b7807ecd
Nuh Aktürk <nuhar42@gmail.com>
2022-01-06 00:16:07 +0300

    3. Soru yapildi.

--- C#_101/odev_1/Program.cs
+++ C#_101/odev_1/Program.cs
@@ -6,13 +6,13 @@ namespace odev_1
     {
         static void Main(string[] args)
         {
-            //************** 1. Soru **************
+            //**************************** 1. Soru ****************************
             Console.WriteLine("************** 1. Soru **************\n");
 
             uint n = 0;
             //Sayı girişi
-            n = pozitifSayiAl(n,"Pozitif bir sayı giriniz");
-            Console.WriteLine("{0} adet sayı gireceksiniz!\n",n);
+            n = pozitifSayiAl(n, "Pozitif bir sayı giriniz");
+            Console.WriteLine("Lütfen {0} adet sayı giriniz!\n", n);
 
             //Kullanıcıdan alınan sayıların diziye aktarılması
             uint[] pozSayiDizisi = new uint[n]; //dizi büyüklüğü == n
@@ -23,34 +23,48 @@ namespace odev_1
             Program instance = new();
             instance.diziYazdirCift(pozSayiDizisi);
 
-            //************** 2. Soru **************
+            //**************************** 2. Soru ****************************
             Console.WriteLine("\n************** 2. Soru **************");
 
             n = 0;
             uint m = 0;
             Console.WriteLine("Lütfen 2 adet pozitif sayı giriniz: ");
             //n için sayı girişi
-            n = pozitifSayiAl(n,"1. sayıyı giriniz");
+            n = pozitifSayiAl(n, "1. sayıyı giriniz");
             //m için sayı girişi
-            m = pozitifSayiAl(m,"2. sayıyı giriniz");
-            Console.WriteLine("{0} adet sayı gireceksiniz!\n",n);
-            
+            m = pozitifSayiAl(m, "2. sayıyı giriniz");
+            Console.WriteLine("Lütfen {0} adet sayı giriniz!\n", n);
+
             //Kullanıcıdan alınan sayıların diziye aktarılması
             uint[] pozSayiDizisi2 = new uint[n]; //dizi büyüklüğü == n
             pozSayiDizisi2 = diziyeSayiAl(n, pozSayiDizisi2);
-            
+
             //Dizideki elemanları foreach ile döndürüp m ye eşit yada tam bölünenleri yazdırma.
             Console.WriteLine("\nGirdiğiniz 2. Sayıya eşit yada tam bölünen sayılar: \n");
             instance.diziYazdirETBolunen(pozSayiDizisi2, m);
-            
-            //************** 3. Soru **************
+
+            //**************************** 3. Soru ****************************
             Console.WriteLine("\n************** 3. Soru **************");
 
+            n = 0;
+            //Sayı girişi
+            n = pozitifSayiAl(n, "Pozitif bir sayı giriniz");
+            Console.WriteLine("Lütfen {0} adet kelime giriniz!\n", n);
 
+            //Diziye kelime girişi
+            string[] kelimeler = new string[n]; //dizi büyüklüğü == n
+            kelimeler = diziyeKelimeAl(n, kelimeler);
 
-            //************** 4. Soru **************
+            //Dizi elemanlarını tersine çevirip yazdırma.
+            Array.Reverse(kelimeler);
+            Console.WriteLine("\nTersten yazdırılan girdiğiniz kelimeler: \n");
+            instance.diziYazdirKelime(kelimeler);
+
+            //**************************** 4. Soru ****************************
             Console.WriteLine("\n************** 4. Soru **************");
         }
+        //**************************** Metodlar ****************************
+        //Kullanıcıdan yalnızca pozitif sayı alır.
         public static uint pozitifSayiAl(uint sayi, string cumle)
         {
             do
@@ -72,6 +86,7 @@ namespace odev_1
             return sayi;
         }
 
+        //Dizinin elemanlarına sayı kaydeder.
         public static uint[] diziyeSayiAl(uint diziBuyuklugu, uint[] dizi)
         {
             for (int i = 0; i < diziBuyuklugu; i++)
@@ -97,6 +112,27 @@ namespace odev_1
             return dizi;
         }
 
+        //Dizinin elemanlarına kelime kaydeder.
+        public static string[] diziyeKelimeAl(uint diziBuyuklugu, string[] dizi)
+        {
+            string veri;
+            for (int i = 0; i < diziBuyuklugu; i++)
+            {
+                do
+                {
+                    Console.Write("{0}. Kelimeyi giriniz: ", i + 1);
+                    veri = Console.ReadLine();
+                    if (String.IsNullOrEmpty(veri))
+                    {
+                        Console.WriteLine("Boş Giriş yapamazsınız!\n");
+                    }
+                } while (String.IsNullOrEmpty(veri));
+                dizi[i] = veri;
+            }
+            return dizi;
+        }
+
+        //Dizideki çift sayıları ekrana yazdırır.
         public void diziYazdirCift(uint[] dizi)
         {
             foreach (var cift in dizi)
@@ -108,6 +144,7 @@ namespace odev_1
             }
         }
 
+        //Dizideki eşit veya tam bölünen sayıları ekrana yazdırır.
         public void diziYazdirETBolunen(uint[] dizi, uint bSayi)
         {
             foreach (var item in dizi)
@@ -116,6 +153,15 @@ namespace odev_1
                 {
                     Console.WriteLine("- " + item);
                 }
+            }
+        }
+
+        //Dizideki elemanları yazdırır.
+        public void diziYazdirKelime(string[] dizi)
+        {
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
             }
         }
     }
