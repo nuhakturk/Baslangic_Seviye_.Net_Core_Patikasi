062beac2e9decfc8ea7ac1565bf97aa35279aeb3
Nuh Aktürk <nuhar42@gmail.com>
2022-01-09 01:23:56 +0300

    .NET console projesi oluşturuldu

---
+++ C#_101/hazir_metotlar_datetime_math/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hazir_metotlar_datetime_math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
