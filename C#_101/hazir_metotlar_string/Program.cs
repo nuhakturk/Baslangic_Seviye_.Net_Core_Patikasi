64398eaf573197c9659202c4ff628fd8dbb7c48d
Nuh Aktürk <nuhar42@gmail.com>
2022-01-08 22:04:22 +0300

    .NET console projesi olusturuldu

---
+++ C#_101/hazir_metotlar_string/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hazir_metotlar_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
