4230eefefb9f1b4dfb41220a1efc2919de8fe81b
Nuh Aktürk <nuhar42@gmail.com>
2022-01-11 23:40:36 +0300

    .NET console projesi oluşturuldu

---
+++ C#_101/generic_koleksiyonlar_ve_list/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace generic_koleksiyonlar_ve_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
