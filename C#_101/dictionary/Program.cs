351472570c583133ed71b372b5d9ad274838369b
Nuh Aktürk <nuhar42@gmail.com>
2022-01-12 14:22:08 +0300

    Dictionary örnekleri yapıldı.

--- C#_101/dictionary/Program.cs
+++ C#_101/dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic; //Kütüphanesi eklendi
 
 namespace dictionary
 {
@@ -6,7 +7,55 @@ namespace dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //System.Collection.Generic
+            Dictionary<int,string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(10, "Ayşe Yılmaz");
+            kullanicilar.Add(12, "Ahmet Yılmaz");
+            kullanicilar.Add(18, "Deniz Arda");
+            kullanicilar.Add(20, "Özcan Coşar");
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine("****** Elemanlara Erişim ******");
+            Console.WriteLine(kullanicilar[12]);
+            
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Count
+            Console.WriteLine("****** Count ******");
+            Console.WriteLine(kullanicilar.Count);
+
+            //Contains
+            Console.WriteLine("****** Contains ******");
+            Console.WriteLine(kullanicilar.ContainsKey(12));
+            Console.WriteLine(kullanicilar.ContainsValue("Nuh Aktürk"));
+
+            //Remove
+            Console.WriteLine("****** Remove ******");
+            kullanicilar.Remove(12);
+
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            
+            //Keys
+            Console.WriteLine("****** Keys ******");
+            foreach (var item in kullanicilar.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Values
+            Console.WriteLine("****** Values ******");
+            foreach (var item in kullanicilar.Values)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
