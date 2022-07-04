efe37379efa5e79e2f2f446996ef8507b51a63f0
Nuh Akturk <nuhar42@gmail.com>
2022-07-04 21:55:34 +0300

    .NET console projesi olusturuldu

---
+++ C#_101/odev_2/Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collection;
+
+namespace Koleksiyonlar_Soru_1
+{
+    class Program
+    {
+        ArrayList sayiListesi = new ArrayList();
+        for (int i = 0; i < 20; i++)
+        {
+            
+        }
+    }
+}
