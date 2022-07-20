0e60913430f03a10e11829dd6548f4c07a4cef29
Nuh Akturk <nuhar42@gmail.com>
2022-07-20 18:43:50 +0300

    Uygulama yapildi

--- C#_101/Siniflar/Sinif-Kavrami-2/Program.cs
+++ C#_101/Siniflar/Sinif-Kavrami-2/Program.cs
@@ -6,7 +6,71 @@ namespace Sinif_Kavrami_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Söz Dizimi
+            // class SinifAdi
+            // {
+            //     [Erişim Belirleyici] [Veri Tipi] OzellikAdi;
+            //     [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi([Parametre Listesi])
+            //     {
+            //         //Metot Komutları
+            //     }
+            // }
+
+            //Erişim Belirleyiciler
+            // * Public = Her yerden erişilebilir.
+            // * Private = Sadece tanımlandığı sınıf içerisinden erişilebilir.
+            // * Internal = Sadece bulunduğu proje içerisinden erişilebilir
+            // * Protected = Sadece tanımlandığı sınıfta ya da o sınıfı miras alan sınıflardan erişilebilir.
+
+            Console.WriteLine("********** Çalışan 1 ************");
+            Calisan calisan1 = new Calisan("Ayşe", "Kara", 23425634, "İnsan Kaynakları");
+            calisan1.CalisanBilgileri();
+            
+            Console.WriteLine("********** Çalışan 2 ************");
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Deniz";
+            calisan2.Soyad = "Arda";
+            calisan2.No = 25646789;
+            calisan2.Departman = "Satın Alma";
+            calisan2.CalisanBilgileri();
+
+            Console.WriteLine("********** Çalışan 3 ************");
+            Calisan calisan3 = new Calisan("Nuh", "Aktürk");
+            calisan3.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public Calisan(string ad, string soyad, int no, string departman)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+        }
+
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+
+        public Calisan()
+        {
+
+        }
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı: {0}", Ad);
+            Console.WriteLine("Çalışanın Soyadı: {0}", Soyad);
+            Console.WriteLine("Çalışanın Numarası: {0}", No);
+            Console.WriteLine("Çalışanın Departmanı: {0}", Departman);
         }
     }
 }
