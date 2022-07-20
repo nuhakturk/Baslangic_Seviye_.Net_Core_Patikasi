4534ba813c9e383b4ef5f935f089805292e71d36
Nuh Akturk <nuhar42@gmail.com>
2022-07-20 17:19:09 +0300

    .NET console projesi acildi

---
+++ C#_101/Siniflar/Sinif-Kavrami-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sinif_Kavrami_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
