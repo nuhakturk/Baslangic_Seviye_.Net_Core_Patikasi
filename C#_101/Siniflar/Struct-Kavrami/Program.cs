75a751672a09e804d7a663ee2888b1d6d87bef67
Nuh Akturk <nuhar42@gmail.com>
2022-07-21 15:32:10 +0300

    .NET projesi acilip uygulama yapildi

---
+++ C#_101/Siniflar/Struct-Kavrami/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Struct_Kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+
+            Console.WriteLine("Class Alan Hesabı   : {0}", dikdortgen.AlanHesapla());
+
+            //Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct();
+            //Dikdortgen_Struct dikdortgen_Struct;
+            //dikdortgen_Struct.KisaKenar = 3;
+            //dikdortgen_Struct.UzunKenar = 4;
+            Dikdortgen_Struct dikdortgen_Struct = new Dikdortgen_Struct(3,4);
+            
+            Console.WriteLine("Class Alan Hesabı   : {0}", dikdortgen_Struct.AlanHesapla());
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen()
+        {
+            KisaKenar = 3;
+            UzunKenar = 4;
+        }
+
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+        {
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar;
+        }
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+}
