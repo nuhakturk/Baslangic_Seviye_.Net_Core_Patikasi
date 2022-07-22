daf2cc86f36acdda6f5813f9e5185d9deab3f2bb
Nuh Akturk <nuhar42@gmail.com>
2022-07-22 19:05:39 +0300

    .NET projesi acildi ve uygulama yapildi

---
+++ C#_101/Nesneye-Dayali-Programlama/arayuzler/FileLogger.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace arayuzler
+{
+    public class FileLogger : ILogger
+    {
+        public void WriteLog()
+        {
+            Console.WriteLine("Dosyaya log yazar.");
+        }
+    }
+}
