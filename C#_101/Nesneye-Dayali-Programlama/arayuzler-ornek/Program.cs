1f260b808f91ebc3a5b65a18ea933ee227bf099c
Nuh Akturk <nuhar42@gmail.com>
2022-07-22 19:25:39 +0300

    .NET projesi acildi ve uygulama yapildi

---
+++ C#_101/Nesneye-Dayali-Programlama/arayuzler-ornek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace arayuzler_ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Focus focus = new Focus();
+            Console.WriteLine(focus.HangiMarkaninAraci());
+            Console.WriteLine(focus.KacTekerlektenOlusur());
+            Console.WriteLine(focus.StandartRengiNe());
+
+            Civic civic = new Civic();
+            Console.WriteLine(civic.HangiMarkaninAraci());
+            Console.WriteLine(civic.KacTekerlektenOlusur());
+            Console.WriteLine(civic.StandartRengiNe());
+
+            Corolla corolla = new Corolla();
+            Console.WriteLine(corolla.HangiMarkaninAraci());
+            Console.WriteLine(corolla.KacTekerlektenOlusur());
+            Console.WriteLine(corolla.StandartRengiNe());
+        }
+    }
+}
