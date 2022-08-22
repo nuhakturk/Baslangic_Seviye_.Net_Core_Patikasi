5c0b9cacb1ec4cc107975727c57cf9d83a61e2d7
Nuh Akturk <nuhar42@gmail.com>
2022-08-22 14:00:04 +0300

    proje yapildi

---
+++ C#_Projeleri/Ortalama_Hesaplama/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ortalama_Hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int derinlik;
+            Console.Write("Fibonacci dizisinin derinliği ne kadar olsun = ");
+            derinlik = Convert.ToInt32(Console.ReadLine());
+            FibonacciOrt(derinlik);
+        }
+
+        static void FibonacciOrt(int derinlik)
+        {
+            int a = 0;
+            int b = 1;
+            Console.Write("{0} {1} ",a ,b);
+            int toplam = a + b;
+
+            for (int i = 3; i <= derinlik; i++)
+            {
+                int c = a + b;
+                Console.Write("{0} ",c);
+                toplam += c;
+                a = b;
+                b = c;
+            }
+            Console.WriteLine("\nOrtalama: {0}",toplam/derinlik);
+        }
+    }
+}
